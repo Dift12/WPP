95670dd6d5a1aa46435301d61667f60ca38b9650
dift12 <diftbanda@gmail.com>
2024-05-29 18:16:20 -0300

    Make menu css and page

---
+++ src/pages/menu/menu.js
@@ -0,0 +1,39 @@
+import "./menu.lazy.css";
+
+export default function makeMenu(){
+    
+    const section=document.createElement("section");
+    section.classList.add("menu");
+    const menu=document.createElement("ul");
+
+    
+    for(let i=0;i<18;i++){
+        const name=document.createElement("p");
+        const dish=document.createElement("li");
+        const ingredients=document.createElement("p");
+        ingredients.classList.add("ingredients");
+        const description=document.createElement("p");
+        description.classList.add("description");
+        
+        const card=document.createElement("div");
+        card.classList.add("card");
+
+        name.textContent="Lorem a la mode";
+        ingredients.textContent="Lorem, Ipsum, Ice Cream, Grapes, Wine, Garlic and Chicken";
+        description.textContent="Le petit bullshit that i dont know how to fill, all mixed to perfection to create a increible flavor flave."
+
+        card.appendChild(name);
+        card.appendChild(ingredients);
+        card.appendChild(description);
+
+        dish.appendChild(card);
+
+        menu.appendChild(dish);
+        
+    }
+
+    
+    section.appendChild(menu);
+    return section;
+
+}
