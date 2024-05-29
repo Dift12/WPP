e55999bf09bd76a8a5be2212ce1328ea607bd4d2
dift12 <diftbanda@gmail.com>
2024-05-29 18:14:40 -0300

    Append menu to content for testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 
 
-import makeHome from "./pages/home/home";
+//import makeHome from "./pages/home/home";
+import makeMenu from "./pages/menu/menu";
 import "./shared-css/shared.css";
 
 const content=document.querySelector("#content");
 console.log(content);
-content.appendChild(makeHome());
+content.appendChild(makeMenu());
 
