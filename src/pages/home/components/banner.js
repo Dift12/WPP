7a24e98d9cd0737b43c7a84e6d552bdd379a9594
dift12 <diftbanda@gmail.com>
2024-05-29 16:22:30 -0300

    Modify banner component

--- src/pages/home/components/banner.js
+++ src/pages/home/components/banner.js
@@ -1,20 +1,34 @@
+import media from "../video.mp4";
+
 export default function banner(){
     const banner=document.createElement("section");
     
     const title=document.createElement("p");
     const subTitle=document.createElement("p");
+    const overlay=document.createElement("div");
+    const video=document.createElement("video");
 
     const button=document.createElement("button");
 
     banner.classList.add("banner");
+    overlay.classList.add("ban-overlay");
 
     title.textContent="What do you think it is?";
     subTitle.textContent="A cool banner, work with us or something";
     button.textContent="Hover me!";
 
-    banner.appendChild(title);
-    banner.appendChild(subTitle);
-    banner.appendChild(button);
+    video.src=media;
+    video.loop=true;
+    video.autoplay=true;
+    video.controls=false;
+    video.muted=true;
+    
+
+    overlay.appendChild(title);
+    overlay.appendChild(subTitle);
+    overlay.appendChild(button);
+    banner.appendChild(video);
+    banner.appendChild(overlay);
 
     return banner;
 }
