b5442c260c6563165237ce03f9d5ac2fd654d29e
dift12 <diftbanda@gmail.com>
2024-05-29 17:09:31 -0300

    Add review component to homepage

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,8 @@
 import hero from "./components/hero";
 import info from "./components/info";
 import banner from "./components/banner";
+import review from "./components/review";
+
 import "./home.css";
 
 export default function makeHome(){
@@ -8,5 +10,6 @@ export default function makeHome(){
     home.appendChild(hero());
     home.appendChild(info());
     home.appendChild(banner());
+    home.appendChild(review());
     return home;
 }
